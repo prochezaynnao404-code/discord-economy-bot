98e21633c554c81a0c25ca803aede1bc0da7e01f
Yami <pro.chezaynnao404@gmail.com>
2026-05-29 12:19:39 +0200

    fix leardboard

--- slashCommands/leaderboard.js
+++ slashCommands/leaderboard.js
@@ -1,146 +1,33 @@
-const {
-    SlashCommandBuilder,
-    EmbedBuilder
-} = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-
-    data: new SlashCommandBuilder()
-
-        .setName("daily")
-
-        .setDescription(
-            "Récupère ton daily"
-        ),
-
     async execute(interaction) {
+        await interaction.deferReply();
 
-        const db =
-            interaction.client.db;
+        const db = interaction.client.db;
 
-        const userId =
-            interaction.user.id;
-
-        db.get(
-            "SELECT * FROM users WHERE userId = ?",
-            [userId],
-
-            (err, user) => {
-
-                if (!user) {
-
-                    db.run(
-                        `
-                        INSERT INTO users (
-                            userId
-                        )
-                        VALUES (?)
-                        `,
-                        [userId]
-                    );
-
-                    return interaction.reply({
-                        content: "Réessaie la commande.",
-                        flags: 64
+        db.all(
+            "SELECT * FROM users ORDER BY money DESC LIMIT 10",
+            [],
+            async (err, rows) => {
+                if (err) {
+                    return interaction.editReply({
+                        content: "❌ Erreur leaderboard."
                     });
                 }
 
-                const now =
-                    Date.now();
+                let description = "";
+                rows.forEach((user, index) => {
+                    description += `${index + 1}. <@${user.userId}> — ${user.money}$\n`;
+                });
 
-                const cooldown =
-                    86400000;
+                const embed = new EmbedBuilder()
+                    .setTitle("🏆 Leaderboard")
+                    .setColor("Gold")
+                    .setDescription(description);
 
-                // COOLDOWN
-
-                if (
-                    user.lastDaily
-                    && now - user.lastDaily
-                    < cooldown
-                ) {
-
-                    const timeLeft =
-                        cooldown -
-                        (
-                            now
-                            - user.lastDaily
-                        );
-
-                    const hours =
-                        Math.floor(
-                            timeLeft / 3600000
-                        );
-
-                    const minutes =
-                        Math.floor(
-                            (
-                                timeLeft
-                                % 3600000
-                            ) / 60000
-                        );
-
-                    return interaction.reply({
-                        embeds: [
-                            new EmbedBuilder()
-                                .setColor("Red")
-                                .setTitle("⏳ DAILY")
-                                .setDescription(`Reviens dans\n${hours}h ${minutes}m`)
-                        ],
-                        flags: 64
-                    });
-                }
-
-                // STREAK
-
-                let streak =
-                    (
-                        user.dailyStreak
-                        || 0
-                    ) + 1;
-
-                let reward =
-                    5000;
-
-                // BONUS X2
-
-                if (
-                    streak >= 10
-                ) {
-
-                    reward *= 2;
-
-                    streak = 0;
-                }
-
-                const newMoney =
-                    user.money + reward;
-
-                db.run(
-                    `
-                    UPDATE users
-
-                    SET money = ?,
-                    lastDaily = ?,
-                    dailyStreak = ?
-
-                    WHERE userId = ?
-                    `,
-                    [
-                        newMoney,
-                        now,
-                        streak,
-                        userId
-                    ]
-                );
-
-                 interaction.reply({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setColor("Green")
-                            .setTitle("🎁 DAILY")
-                            .setDescription(`💰 Gain :\n${reward}$\n\n🔥 Série :\n${streak}/10`)
-                    ],
-                    flags: 64
+                await interaction.editReply({
+                    embeds: [embed]
                 });
             }
         );
